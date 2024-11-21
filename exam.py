bb1b34cd73ac894ebe69ede2af8cf20cacef5be6
pro <prospersassaa@gmail.com>
2024-11-21 10:35:42 +0200

    exercise

---
+++ exam.py
@@ -0,0 +1,27 @@
+import math
+from array import *
+
+ans='Y'
+choice=0
+num1=0
+num2=0
+nres=0
+op=""
+distn={}
+ar=array('f', [])
+index=0
+
+while ans=='Y' or ans=='y':
+    print("1. Sum + (x,y)")
+    print("2. Difference - (x,y)")
+    print("3. Multiplication * (x,y)")
+    print("4. Division / (x,y)")
+    print("5. Sqrt (x)")
+    print("6. Pow  (x,y)")
+    print("7. Exponent ex (x)")
+    
+    choice=int (input("Enter your choice from the menu: "))
+    if choice==5:
+    
+    choice=int(input("Enter your choice from the menu: ")) 
+    
